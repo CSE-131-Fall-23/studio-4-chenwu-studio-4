5a439dc0d98f8b36feafc09765c46f61e4f355aa
lanxyi.chz <lanxyi.chz@10.232.129.36>
2023-10-04 14:13:10 -0500

    Studio 4

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,49 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();	
+		
+		if (shapeType.equals("rectangle")) {
+			StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			
+			if(isFilled) {
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		
+		if (shapeType.equals("triangle")) {
+			double parameterFive = in.nextDouble();
+			double parameterSix = in.nextDouble();
+			
+			double[] x = {parameterOne, parameterThree, parameterFive};
+			double[] y = {parameterTwo, parameterFour, parameterSix};
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			StdDraw.polygon(x, y);
+			
+			if(isFilled) {
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				StdDraw.filledPolygon(x, y);
+			}
+		}
+		
+		if (shapeType.equals("ellipse")) {
+			
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			
+			if (isFilled) {
+				StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
 	}
 }
